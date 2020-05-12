a8a45cf11b1d57542d75475cebb16d834b3b4958
kawada13 <vgnsl94@gmail.com>
2020-05-12 08:58:21 +0000

    init

---
+++ app/Http/Controllers/LineBotController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LineBotController extends Controller
+{
+  public function index()
+  {
+      return view('linebot.index');
+  }
+
+  
+}
---
+++ resources/views/linebot/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>laravel-linebot</title>
+</head>
+<body>
+  <h1>世間は広いなあ！！！！！！大きいなあ！！</h1>
+</body>
+</html>
